c09a9f5377ff685ab5e44a26e696f258800bd5c5
SatyaManoj12 <119724289+SatyaManoj12@users.noreply.github.com>
2023-05-24 18:44:55 +0530

    N = int(input()) f3 = 0 f1 = 1 f2 = 1 if (N == 0 or N == 1):     print("True")
    else:
        while f3 < N:
            f3 = f1 + f2
            f2 = f1
            f1 = f3
        if f3 == N:
            print("True")
        else:
            print("False")

--- Fibonacci_or_not.py
+++ Fibonacci_or_not.py
@@ -1,15 +1,16 @@
-n=int(input())
-f1=0
-f2=1
-f3=1
-if n==0 or n==1:
-    printf("True")
+N = int(input())
+f3 = 0
+f1 = 1
+f2 = 1
+if (N == 0 or N == 1):
+    print("True")
+
 else:
-    while f3<n:
-        f3=f1+f2
-        f2=f1
-        f1=f3
-    if f3==n:
+    while f3 < N:
+        f3 = f1 + f2
+        f2 = f1
+        f1 = f3
+    if f3 == N:
         print("True")
     else:
         print("False")
